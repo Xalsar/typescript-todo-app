a23216b4ff0203df7f0f14987eeb058c8483744c
Xavi <xalsar@protonmail.com>
2021-05-11 15:52:05 +0200

    Create Todos

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+import Todos from "./components/Todos/Todos";
+
 ReactDOM.render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+    <Todos items={["Hacer cosas", "Hacer mas cosas"]} />
+  </React.StrictMode>,
   document.getElementById("root")
 );
